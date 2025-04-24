310baabc349bbd3a15fb372a4d88ab688af8678b
Ellie Alavi <elhamalavi.f@gmail.com>
2025-04-24 04:28:03 -0400

    database backed model created

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from .db import db, migrate
+from .models import book 
 from .routes.book_route import books_bp
 
 def create_app():
     app = Flask(__name__)
 
+    # SQLAlchemy settings
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Hide a warning
+
+    # Set the Connection String; Here we tell Flask where to find the new DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    # Registering DB and migrate here
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     # Registering Blueprints here
     # app.register_blueprint(hello_world_bp)
     app.register_blueprint(books_bp)
--- app/routes/book_route.py
+++ app/routes/book_route.py
@@ -1,70 +1,64 @@
-'''
-    Import the Blueprint class from Flask
-    Import the books list (data from our "database") from book.py
-    Create an instance of Blueprint for our routes related to the Book model
-    Register our blueprint with our app
-    Write a route to get all the book records from our "database"
-'''
-# from flask import Blueprint, abort, make_response  # additional imports for refactoring option 2
-from flask import Blueprint
-from app.routes.helper import validate_book # additional import for refatoring option 3
-from app.models.book import books
+from flask import Blueprint, abort, make_response  # additional imports for refactoring option 2
+# from flask import Blueprint
+# from app.routes.helper import validate_book # additional import for refatoring option 3
+# from app.models.book import books
 
 # books_bp = Blueprint("books_bp", __name__)
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books") # I can specify an endpoint as URL_Prefix
 
-# @books_bp.get("/books")  # if I have specified endpoint on line 12 then no need for /books here
-@books_bp.get("")
-def get_books():
+# # Hardcoded Blueprints
+# # @books_bp.get("/books")  # if I have specified endpoint on line 12 then no need for /books here
+# @books_bp.get("")
+# def get_books():
     
-    books_response = []
+#     books_response = []
 
-    for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            })
-        # books_response.append(dict(   # Another way to define a dict
-        #     id = book.id,
-        #     title = book.title,
-        #     description = book.description
-        # ))
+#     for book in books:
+#         books_response.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             })
+#         # books_response.append(dict(   # Another way to define a dict
+#         #     id = book.id,
+#         #     title = book.title,
+#         #     description = book.description
+#         # ))
     
-    return books_response
+#     return books_response
 
-# second endpoint for one book
-@books_bp.get("/<book_id>") # now with this endpoint we need to use line 12 instead of line 11
-def get_one_book(book_id):
+# # second endpoint for one book
+# @books_bp.get("/<book_id>") # now with this endpoint we need to use line 12 instead of line 11
+# def get_one_book(book_id):
 
-    ## Option 1: direct Error Handling
-    # try:
-    #     book_id = int(book_id)
-    # except:
-    #     return {"message": f"book {book_id} invalid"}, 400 # handling invalid book_id
+#     # Option 1: direct Error Handling
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         return {"message": f"book {book_id} invalid"}, 400 # hardcoded handling invalid book_id
 
-    # for book in books:
-    #     if book.id == book_id:
-    #         return {
-    #             "id": book.id,
-    #             "title": book.title,
-    #             "description": book.description,
-    #         }
-    # return {"message": f"book {book_id} not found"}, 404  # handling not found book_id
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             }
+#     return {"message": f"book {book_id} not found"}, 404  # hardcoded handling not found book_id
 
 
-    # Refactoring
-    book = validate_book(book_id)
+#     # # Refactoring
+#     # book = validate_book(book_id)  # Option 3 Error handling
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
-    }
+#     # return {
+#     #     "id": book.id,
+#     #     "title": book.title,
+#     #     "description": book.description,
+#     # }
     
 
-## Option 2: Refactoring and validating in the same routeing file
+# # Option 2: Refactoring and validating in the same routeing file
 # def validate_book(book_id):
 #     try:
 #         book_id = int(book_id)
