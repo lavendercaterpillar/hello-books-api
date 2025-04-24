61f2d43465183b1b6b3bf8854b49bc125c3238b5
Ellie Alavi <elhamalavi.f@gmail.com>
2025-04-24 19:34:19 -0400

    GET Request Configured

--- app/routes/book_route.py
+++ app/routes/book_route.py
@@ -14,13 +14,37 @@ def create_book():
     description = request_body["description"]
 
     new_book = Book(title=title, description=description)
-    db.session.add(new_book)
-    db.session.commit()
 
-    # We need to convert response back to JSON
+    # lines 18 and 19 are responsible for saving a new book to the database.
+    db.session.add(new_book) # Think of it like staging the object—it's marked to be inserted into the database, but it’s not saved yet.
+    db.session.commit() # At this point, the new_book is inserted into the database, and if your Book model has an auto-incrementing ID, it gets assigned right here.
+
+    # We need to convert response body back to JSON
     response = {
         "id": new_book.id,
         "title": new_book.title,
         "description": new_book.description,
     }
     return response, 201
+
+
+# Creating a GET request for retrieving ALL books
+@books_bp.get("")
+def get_all_books():
+    # query = db.select(Book) # similar to SELECT in SQL
+    query = db.select(Book).order_by(Book.id)  # sorted in a particular order by calling order_by  
+    books = db.session.scalars(query) # This method returns a list of instances of Book.
+
+    # We could also write the lines above as:
+    # books = db.session.execute(query).scalars()
+
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
