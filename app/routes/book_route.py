634ebb5f0a64afb8d857e84d2d709e8d58666dd7
Ellie Alavi <elhamalavi.f@gmail.com>
2025-04-28 02:07:12 -0400

    Created SQLAlchemy GET 1 book Request

--- app/routes/book_route.py
+++ app/routes/book_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, make_response, request  # additional imports for refactoring option 2
 from app.models.book import Book
 from ..db import db
+from .helper import validate_book
 
 # books_bp = Blueprint("books_bp", __name__)
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books") 
@@ -15,7 +16,7 @@ def create_book():
 
     new_book = Book(title=title, description=description)
 
-    # lines 18 and 19 are responsible for saving a new book to the database.
+    # lines 18 and 19 are responsible for saving the new book to the database.
     db.session.add(new_book) # Think of it like staging the object—it's marked to be inserted into the database, but it’s not saved yet.
     db.session.commit() # At this point, the new_book is inserted into the database, and if your Book model has an auto-incrementing ID, it gets assigned right here.
 
@@ -33,7 +34,7 @@ def create_book():
 def get_all_books():
     # query = db.select(Book) # similar to SELECT in SQL
     query = db.select(Book).order_by(Book.id)  # sorted in a particular order by calling order_by  
-    books = db.session.scalars(query) # This method returns a list of instances of Book.
+    books = db.session.scalars(query) # This method returns the query which is a list of instances of Book.
 
     # We could also write the lines above as:
     # books = db.session.execute(query).scalars()
@@ -48,3 +49,29 @@ def get_all_books():
             }
         )
     return books_response
+
+
+# # Creating a GET request to retrieve 1 book
+# @books_bp.get("/<book_id>")  # Place holder for book_id endpoint in the GET request
+# def get_one_book(book_id):
+#     # Similar to SELECT <> WHERE <> in SQL
+#     query = db.select(Book).where(Book.id == book_id)
+#     book = db.session.scalar(query) # returns based on the above query (book_id)
+
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description
+#     }
+
+
+# Creating a GET request to retrieve 1 book with Validation
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
--- app/routes/helper.py
+++ app/routes/helper.py
@@ -1,8 +1,26 @@
-# from flask import abort,make_response
-# from app.models.book import books
+from flask import abort,make_response
+from app.models.book import Book
+from ..db import db
+
+# database_backed model validation
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+    
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+
+    return book
 
 
-# ## Option 3: Refactoring and validating in a helper file
+# Option 3: (Hardcode) Refactoring and validating in a helper file
 # def validate_book(book_id):
 #     try:
 #         book_id = int(book_id)
